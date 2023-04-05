ad254f05ad9a3dedc7f1b06fe997d43fbc05e762
GPortas <hey@gportas.me>
2023-04-05 17:15:14 +0100

    Removed: old package code

--- src/old/exceptions/dataverseException.ts
+++
@@ -1,8 +0,0 @@
-export class DataverseException extends Error {
-  public readonly errorCode: number;
-
-  constructor(errorCode: number, message?: string) {
-    super(message);
-    this.errorCode = errorCode;
-  }
-}
