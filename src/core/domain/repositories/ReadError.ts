ef8d69b27d6c552a1ec95aa02796f0e19d0dba30
GPortas <hey@gportas.me>
2023-04-14 09:56:07 +0100

    Refactor: string concat in ReadError

--- src/core/domain/repositories/ReadError.ts
+++ src/core/domain/repositories/ReadError.ts
@@ -2,7 +2,7 @@ export class ReadError extends Error {
   constructor(reason?: string) {
     let message = 'There was an error when reading the resource.';
     if (reason) {
-      message += ' Reason was: ' + reason;
+      message += ` Reason was: ${reason}`;
     }
     super(message);
   }
