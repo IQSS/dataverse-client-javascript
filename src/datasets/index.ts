056e34f12e5f2ed490a007f5724224df3413b638
GPortas <hey@gportas.me>
2023-05-18 15:54:46 +0100

    Added: dataset models export

--- src/datasets/index.ts
+++ src/datasets/index.ts
@@ -4,3 +4,13 @@ import { GetDatasetSummaryFieldNames } from './domain/useCases/GetDatasetSummary
 const getDatasetSummaryFieldNames = new GetDatasetSummaryFieldNames(new DatasetsRepository());
 
 export { getDatasetSummaryFieldNames };
+export {
+  Dataset,
+  DatasetVersionInfo,
+  DatasetVersionState,
+  DatasetLicense,
+  DatasetMetadataBlock,
+  DatasetMetadataFields,
+  DatasetMetadataFieldValue,
+  DatasetMetadataSubField,
+} from './domain/models/Dataset';
