be914ac1cacef7b7ca9c7a70b9459a4b5fc400e3
GPortas <hey@gportas.me>
2023-05-26 09:49:08 +0100

    Fixed: removed deleted error model from exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,3 @@
 export { ReadError } from './domain/repositories/ReadError';
 export { WriteError } from './domain/repositories/WriteError';
-export { MissingParameterError } from './domain/repositories/MissingParameterError';
 export { ApiConfig } from './infra/repositories/ApiConfig';
