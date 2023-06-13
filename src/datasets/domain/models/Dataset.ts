e3a3084742cc8d76ff1faf49771a6318ff691788
MellyGray <melinaabril@gmail.com>
2023-06-13 11:54:39 +0200

    feat: add all the possible citation metadata fields

--- src/datasets/domain/models/Dataset.ts
+++ src/datasets/domain/models/Dataset.ts
@@ -36,20 +36,59 @@ export interface DatasetMetadataBlock {
   fields: DatasetMetadataFields;
 }
 
+export type DatasetMetadataFields = Record<string, DatasetMetadataFieldValue>;
+
+export type DatasetMetadataFieldValue = string | string[] | DatasetMetadataSubField | DatasetMetadataSubField[];
+
+export type DatasetMetadataSubField = Record<string, string>;
+
 export interface CitationMetadataBlock extends DatasetMetadataBlock {
   name: 'citation';
   fields: {
     title: string;
-    author: DatasetAuthor[];
+    author: Author[];
     datasetContact: DatasetContact[];
     dsDescription: DatasetDescription[];
     subject: string[];
+    subtitle?: string;
+    alternativeTitle?: string;
+    alternativeURL?: string;
+    otherId?: OtherId[];
+    keyword?: Keyword[];
+    topicClassification?: TopicClassification[];
+    publication?: Publication[];
+    notesText?: string;
+    language?: string[];
+    producer?: Producer[];
+    productionDate?: string;
+    productionPlace?: string[];
+    contributor?: Contributor[];
+    grantNumber?: GrantNumber[];
+    distributor?: Distributor[];
+    distributionDate?: string;
     depositor?: string;
     dateOfDeposit?: string;
+    timePeriodCovered?: TimePeriodCovered[];
+    dateOfCollection?: DateOfCollection[];
+    kindOfData?: string[];
+    series?: Series[];
+    software?: Software[];
+    relatedMaterial?: string[];
+    relatedDatasets?: string[];
+    otherReferences?: string[];
+    dataSources?: string[];
+    originOfSources?: string;
+    characteristicOfSources?: string;
+    accessToSources?: string;
   };
 }
 
-export interface DatasetAuthor extends DatasetMetadataSubField {
+interface OtherId extends DatasetMetadataSubField {
+  otherIdAgency?: string;
+  otherIdValue?: string;
+}
+
+export interface Author extends DatasetMetadataSubField {
   authorName: string;
   authorAffiliation: string;
   authorIdentifierScheme?: string;
@@ -64,10 +103,70 @@ export interface DatasetContact extends DatasetMetadataSubField {
 
 export interface DatasetDescription extends DatasetMetadataSubField {
   dsDescriptionValue: string;
+  dsDescriptionDate?: string;
 }
 
-export type DatasetMetadataFields = Record<string, DatasetMetadataFieldValue>;
+interface Keyword extends DatasetMetadataSubField {
+  keywordValue?: string;
+  keywordVocabulary?: string;
+  keywordVocabularyURI?: string;
+}
 
-export type DatasetMetadataFieldValue = string | string[] | DatasetMetadataSubField | DatasetMetadataSubField[];
+interface TopicClassification extends DatasetMetadataSubField {
+  topicClassValue?: string;
+  topicClassVocab?: string;
+  topicClassVocabURI?: string;
+}
 
-export type DatasetMetadataSubField = Record<string, string>;
+interface Publication extends DatasetMetadataSubField {
+  publicationCitation?: string;
+  publicationIDType?: string;
+  publicationIDNumber?: string;
+  publicationURL?: string;
+}
+
+interface Producer extends DatasetMetadataSubField {
+  producerName?: string;
+  producerAffiliation?: string;
+  producerAbbreviation?: string;
+  producerURL?: string;
+  producerLogoURL?: string;
+}
+
+interface Contributor extends DatasetMetadataSubField {
+  contributorType?: string;
+  contributorName?: string;
+}
+
+interface GrantNumber extends DatasetMetadataSubField {
+  grantNumberAgency?: string;
+  grantNumberValue?: string;
+}
+
+interface Distributor extends DatasetMetadataSubField {
+  distributorName?: string;
+  distributorAffiliation?: string;
+  distributorAbbreviation?: string;
+  distributorURL?: string;
+  distributorLogoURL?: string;
+}
+
+interface TimePeriodCovered extends DatasetMetadataSubField {
+  timePeriodCoveredStart?: string;
+  timePeriodCoveredEnd?: string;
+}
+
+interface DateOfCollection extends DatasetMetadataSubField {
+  dateOfCollectionStart?: string;
+  dateOfCollectionEnd?: string;
+}
+
+interface Series extends DatasetMetadataSubField {
+  seriesName?: string;
+  seriesInformation?: string;
+}
+
+interface Software extends DatasetMetadataSubField {
+  softwareName?: string;
+  softwareVersion?: string;
+}
