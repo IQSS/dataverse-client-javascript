33671e5aebdd4e781de864be15970e0628524734
GPortas <hey@gportas.me>
2023-08-29 12:13:47 +0100

    Fixed: missing FileUserPermissions export added to index

--- src/files/index.ts
+++ src/files/index.ts
@@ -14,6 +14,7 @@ const getFileDataTables = new GetFileDataTables(filesRepository);
 export { getDatasetFiles, getFileDownloadCount, getFileUserPermissions, getFileDataTables };
 
 export { File, FileEmbargo, FileChecksum } from './domain/models/File';
+export { FileUserPermissions } from './domain/models/FileUserPermissions';
 export { FileCriteria, FileOrderCriteria, FileAccessStatus } from './domain/models/FileCriteria';
 export {
   FileDataTable,
