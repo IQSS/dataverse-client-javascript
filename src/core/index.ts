31499d7d603643f1742f066c4868fbebe2549324
MellyGray <melinaabril@gmail.com>
2024-02-28 07:58:30 +0000

    feat(OwnersInfo): export DvObjectType interface

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,4 @@
 export { ReadError } from './domain/repositories/ReadError';
 export { WriteError } from './domain/repositories/WriteError';
 export { ApiConfig } from './infra/repositories/ApiConfig';
-export { DvObjectOwnerNode } from './domain/models/DvObjectOwnerNode';
+export { DvObjectOwnerNode, DvObjectType } from './domain/models/DvObjectOwnerNode';
