fd4cfeaa772908274cb636af251059c4d2951d4c
MellyGray <melinaabril@gmail.com>
2024-03-26 16:46:15 +0100

    feat(PublishDataset use case): add new PublishDataset use case

---
+++ src/datasets/domain/useCases/PublishDataset.ts
@@ -0,0 +1,22 @@
+import { UseCase } from '../../../core/domain/useCases/UseCase'
+import { VersionUpdateType } from '../models/Dataset'
+import { IDatasetsRepository } from '../repositories/IDatasetsRepository' // Assuming Axios for HTTP requests
+
+export class PublishDataset implements UseCase<void> {
+  private datasetsRepository: IDatasetsRepository
+
+  constructor(datasetsRepository: IDatasetsRepository) {
+    this.datasetsRepository = datasetsRepository
+  }
+
+  /**
+   * Publishes a dataset, given its identifier and the type of version update type.
+   *
+   * @param {number | string} [datasetId] - The dataset identifier, which can be a string (for persistent identifiers), or a number (for numeric identifiers).
+   * @param {VersionUpdateType} versionUpdateType - Specifies the type of version update, 'major' or 'minor'.
+   * @returns {Promise<void>} - This method does not return anything upon successful completion.
+   */
+  async execute(datasetId: number | string, versionUpdateType: VersionUpdateType): Promise<void> {
+    return await this.datasetsRepository.publishDataset(datasetId, versionUpdateType)
+  }
+}
