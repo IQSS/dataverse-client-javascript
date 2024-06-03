82e396c1eb9df117b21f4820e4a8b129f929cf6e
GPortas <hey@gportas.me>
2024-06-03 10:00:03 +0100

    Changed: test timeout to original value

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   coveragePathIgnorePatterns: ['node_modules', 'testHelpers'],
   globalSetup: '<rootDir>/test/environment/setup.ts',
-  testTimeout: 120000,
+  testTimeout: 25000,
   coverageThreshold: {
     global: {
       branches: 95,
