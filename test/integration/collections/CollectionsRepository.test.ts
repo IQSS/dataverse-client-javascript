eee6302235059a3ee0a86ebc7962688dc51360c9
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-07-26 12:56:54 -0400

    Update CollectionsRepository.test.ts
    Remove console.log()

--- test/integration/collections/CollectionsRepository.test.ts
+++ test/integration/collections/CollectionsRepository.test.ts
@@ -109,7 +109,6 @@ describe('CollectionsRepository', () => {
       expect(typeof actualId).toBe('number')
 
       const createdCollection = await sut.getCollection(actualId)
-      console.log('returned collection')
       expect(createdCollection.id).toBe(actualId)
       expect(createdCollection.alias).toBe(newCollectionDTO.alias)
       expect(createdCollection.name).toBe(newCollectionDTO.name)
