1429417a68b9b108e93dc0af1ebd3304e189e49c
GPortas <hey@gportas.me>
2024-07-31 13:08:59 +0100

    Added: functional test for GetAllFacetableDatasetFields

---
+++ test/functional/metadataBlocks/GetAllFacetableDatasetFields.test.ts
@@ -0,0 +1,26 @@
+import { ApiConfig, MetadataFieldInfo, getAllFacetableDatasetFields } from '../../../src'
+import { TestConstants } from '../../testHelpers/TestConstants'
+import { DataverseApiAuthMechanism } from '../../../src/core/infra/repositories/ApiConfig'
+
+describe('execute', () => {
+  beforeAll(async () => {
+    ApiConfig.init(
+      TestConstants.TEST_API_URL,
+      DataverseApiAuthMechanism.API_KEY,
+      process.env.TEST_API_KEY
+    )
+  })
+
+  test('should return all facetable dataset fields', async () => {
+    let metadataFieldInfos: MetadataFieldInfo[] = null
+    try {
+      metadataFieldInfos = await getAllFacetableDatasetFields.execute()
+    } catch (error) {
+      throw new Error('Should not raise an error')
+    } finally {
+      expect(metadataFieldInfos.length).toBe(59)
+      expect(metadataFieldInfos[0].name).toBe('authorName')
+      expect(metadataFieldInfos[0].displayName).toBe('Author Name')
+    }
+  })
+})
