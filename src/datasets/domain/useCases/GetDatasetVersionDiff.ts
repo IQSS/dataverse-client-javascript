a8fa2cb6e07c75b1844ea71a3502ab51bf0b6743
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-11-14 09:25:56 -0500

    fix: function comments

--- src/datasets/domain/useCases/GetDatasetVersionDiff.ts
+++ src/datasets/domain/useCases/GetDatasetVersionDiff.ts
@@ -10,12 +10,10 @@ export class GetDatasetVersionDiff implements UseCase<DatasetVersionDiff> {
   }
 
   /**
-   * Returns a Dataset instance, given the search parameters to identify it.
-   *  TODO: should we allow DatasetNotNumberedVersion enum values for newVersionId and oldVersionId?
+   * Returns a DatasetVersionDiff instance, which contains the differences between the two given versions.
    * @param {number | string} [datasetId] - The dataset identifier, which can be a string (for persistent identifiers), or a number (for numeric identifiers).
-   * @param {string } [newVersionId] - The dataset version identifier, which can be a version-specific numeric string (for example, 1.0) or a DatasetNotNumberedVersion enum value. If this parameter is not set, the default value is: DatasetNotNumberedVersion.LATEST
-   * @param {string } [oldVersionId] - The dataset version identifier, which can be a version-specific numeric string (for example, 1.0) or a DatasetNotNumberedVersion enum value. If this parameter is not set, the default value is: DatasetNotNumberedVersion.LATEST * @param {boolean} [includeDeaccessioned=false] - Indicates whether to consider deaccessioned versions in the dataset search or not. The default value is false
-   * @returns {Promise<DatasetVersionDiff>}
+   * @param {string } [newVersionId] - The dataset version identifier, which can be a version-specific numeric string (for example, 1.0) or a DatasetNotNumberedVersion enum value.
+   * @param {string } [oldVersionId] - The dataset version identifier, which can be a version-specific numeric string (for example, 1.0) or a DatasetNotNumberedVersion enum value.
    */
   async execute(
     datasetId: number | string,
