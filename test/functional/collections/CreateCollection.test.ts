e82bb0f1ec89bdb4253951d0987664d5583ca1bc
Germán Saracca <gersaracca@gmail.com>
2025-01-16 15:45:06 -0300

    one more fix type error

--- test/functional/collections/CreateCollection.test.ts
+++ test/functional/collections/CreateCollection.test.ts
@@ -34,7 +34,7 @@ describe('execute', () => {
       await createCollection.execute(testNewCollection, TestConstants.TEST_DUMMY_COLLECTION_ID)
       throw new Error('Use case should throw an error')
     } catch (error) {
-      writeError = error
+      writeError = error as WriteError
     } finally {
       expect(writeError).toBeInstanceOf(WriteError)
       expect(writeError?.message).toEqual(
