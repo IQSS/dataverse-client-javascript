c833344b9962876275ca286532c393e05930eff4
Germán Saracca <gersaracca@gmail.com>
2025-02-21 10:11:55 -0300

    test: add unit cases

---
+++ test/unit/files/ReplaceFile.test.ts
@@ -0,0 +1,34 @@
+import { IFilesRepository } from '../../../src/files/domain/repositories/IFilesRepository'
+import { UploadedFileDTO } from '../../../src/files'
+import { ReplaceFile } from '../../../src/files/domain/useCases/ReplaceFile'
+import { WriteError } from '../../../src/core'
+
+describe('execute', () => {
+  const testUploadedFileDTO: UploadedFileDTO = {
+    fileName: 'testfile',
+    storageId: 'testStorageId',
+    checksumValue: 'testChecksumValue',
+    checksumType: 'md5',
+    mimeType: 'test/type'
+  }
+
+  test('should return undefined on client success', async () => {
+    const filesRepositoryStub: IFilesRepository = {} as IFilesRepository
+    filesRepositoryStub.replaceFile = jest.fn().mockResolvedValue(undefined)
+
+    const sut = new ReplaceFile(filesRepositoryStub)
+
+    const actual = await sut.execute(1, testUploadedFileDTO)
+
+    expect(actual).toEqual(undefined)
+  })
+
+  test('should return error on client error', async () => {
+    const filesRepositoryStub: IFilesRepository = {} as IFilesRepository
+    filesRepositoryStub.replaceFile = jest.fn().mockRejectedValue(new WriteError('Some error'))
+
+    const sut = new ReplaceFile(filesRepositoryStub)
+
+    await expect(sut.execute(1, testUploadedFileDTO)).rejects.toThrow(WriteError)
+  })
+})
