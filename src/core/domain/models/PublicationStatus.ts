c60a7d9d4b12a7fc9b44372f51a9e757707b0395
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-06-03 09:45:50 -0400

    keep PublicationStatus enum in the same order

--- src/core/domain/models/PublicationStatus.ts
+++ src/core/domain/models/PublicationStatus.ts
@@ -1,7 +1,7 @@
 export enum PublicationStatus {
-  Deaccessioned = 'Deaccessioned',
-  InReview = 'In Review',
   Published = 'Published',
   Unpublished = 'Unpublished',
-  Draft = 'Draft'
+  Draft = 'Draft',
+  Deaccessioned = 'Deaccessioned',
+  InReview = 'In Review'
 }
