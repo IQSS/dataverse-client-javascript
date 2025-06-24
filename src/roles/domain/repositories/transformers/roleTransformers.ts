2854a5cefb52a1780c6b2642653d4496aaa7d45f
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-06-24 15:25:21 -0400

    remove debug logs

--- src/roles/domain/repositories/transformers/roleTransformers.ts
+++ src/roles/domain/repositories/transformers/roleTransformers.ts
@@ -2,9 +2,7 @@ import { AxiosResponse } from 'axios'
 import { Role } from '../../models/Role'
 
 export const transformRolesUserSelectableResponseToRoles = (response: AxiosResponse): Role[] => {
-  console.log('transformRolesUserSelectableResponseToRoles', response)
   const roleUserSelectablePayload = response.data.data
-  console.log('transformRolesUserSelectableResponseToRoles', response.data.data)
 
   return roleUserSelectablePayload.map((role: any) => ({
     id: role.id,
