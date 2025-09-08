1331a81f32c38df461ec1b13a2964d6933fbb9f0
Germán Saracca <gersaracca@gmail.com>
2025-09-08 14:55:41 -0300

    feat: use case and docs

--- src/info/index.ts
+++ src/info/index.ts
@@ -3,6 +3,7 @@ import { GetDataverseVersion } from './domain/useCases/GetDataverseVersion'
 import { GetZipDownloadLimit } from './domain/useCases/GetZipDownloadLimit'
 import { GetMaxEmbargoDurationInMonths } from './domain/useCases/GetMaxEmbargoDurationInMonths'
 import { GetApplicationTermsOfUse } from './domain/useCases/GetApplicationTermsOfUse'
+import { GetAvailableDatasetMetadataExportFormats } from './domain/useCases/GetAvailableDatasetMetadataExportFormats'
 
 const dataverseInfoRepository = new DataverseInfoRepository()
 
@@ -10,10 +11,16 @@ const getDataverseVersion = new GetDataverseVersion(dataverseInfoRepository)
 const getZipDownloadLimit = new GetZipDownloadLimit(dataverseInfoRepository)
 const getMaxEmbargoDurationInMonths = new GetMaxEmbargoDurationInMonths(dataverseInfoRepository)
 const getApplicationTermsOfUse = new GetApplicationTermsOfUse(dataverseInfoRepository)
+const getAvailableDatasetMetadataExportFormats = new GetAvailableDatasetMetadataExportFormats(
+  dataverseInfoRepository
+)
 
 export {
   getDataverseVersion,
   getZipDownloadLimit,
   getMaxEmbargoDurationInMonths,
-  getApplicationTermsOfUse
+  getApplicationTermsOfUse,
+  getAvailableDatasetMetadataExportFormats
 }
+
+export { DatasetMetadataExportFormats } from './domain/models/DatasetMetadataExportFormats'
