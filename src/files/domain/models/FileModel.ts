0f1fd84fa4fbca53bdce1932e2110ea894b72047
Germán Saracca <gersaracca@gmail.com>
2025-09-29 11:36:33 -0300

    remove unused

--- src/files/domain/models/FileModel.ts
+++ src/files/domain/models/FileModel.ts
@@ -1,4 +1,4 @@
-import { DvObjectOwnerNode, DvObjectType } from '../../../core/domain/models/DvObjectOwnerNode'
+import { DvObjectOwnerNode } from '../../../core/domain/models/DvObjectOwnerNode'
 
 export interface FileModel {
   id: number
@@ -51,52 +51,3 @@ export interface FileChecksum {
   type: string
   value: string
 }
-
-const fileModelExample: FileModel = {
-  id: 123,
-  persistentId: 'doi:10.1234/example',
-  name: 'data.csv',
-  pidURL: 'http://example.com/file/123',
-  sizeBytes: 2048,
-  version: 1,
-  description: 'Sample data file',
-  restricted: false,
-  latestRestricted: false,
-  directoryLabel: '/data',
-  datasetVersionId: 1,
-  categories: ['survey', 'demographics'],
-  contentType: 'text/csv',
-  friendlyType: 'CSV',
-  embargo: {
-    dateAvailable: new Date('2024-12-31'),
-    reason: 'Embargo until publication'
-  },
-  storageIdentifier: 's3://bucket/key',
-  originalFormat: 'csv',
-  originalFormatLabel: 'CSV',
-  originalSize: 2048,
-  originalName: 'data_original.csv',
-  UNF: 'UNF:6:abc123',
-  rootDataFileId: 123,
-  previousDataFileId: 122,
-  md5: 'd41d8cd98f00b204e9800998ecf8427e',
-  checksum: {
-    type: 'MD5',
-    value: 'd41d8cd98f00b204e9800998ecf8427e'
-  },
-  metadataId: 456,
-  tabularTags: ['tabular', 'data'],
-  creationDate: '2024-01-01T12:00:00Z',
-  publicationDate: '2024-06-01T12:00:00Z',
-  lastUpdateTime: '2024-06-15T12:34:56Z',
-  deleted: false,
-  tabularData: true,
-  fileAccessRequest: false,
-  isPartOf: {
-    type: 'dataverse' as DvObjectType,
-    displayName: 'My Dataverse',
-    identifier: 'my-dataverse'
-  }
-}
-
-fileModelExample.lastUpdateTime
