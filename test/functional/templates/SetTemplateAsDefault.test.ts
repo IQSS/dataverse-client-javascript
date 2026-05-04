0dccf9934f0854bff942ce91ab57137b0d9ebdb5
ErykKul <eryk.kulikowski@gmail.com>
2026-05-04 19:58:17 +0200

    Isolate set default template functional test

--- test/functional/templates/SetTemplateAsDefault.test.ts
+++ test/functional/templates/SetTemplateAsDefault.test.ts
@@ -9,10 +9,12 @@ import {
 import { CreateTemplateDTO } from '../../../src/templates/domain/dtos/CreateTemplateDTO'
 import { MetadataFieldTypeClass } from '../../../src/metadataBlocks/domain/models/MetadataBlock'
 import { deleteDatasetTemplateViaApi } from '../../testHelpers/datasets/datasetTemplatesHelper'
+import {
+  createCollectionViaApi,
+  deleteCollectionViaApi
+} from '../../testHelpers/collections/collectionHelper'
 
 describe('SetTemplateAsDefault.execute', () => {
-  const collectionIdOrAlias = ':root'
-
   beforeEach(async () => {
     ApiConfig.init(
       TestConstants.TEST_API_URL,
@@ -23,6 +25,7 @@ describe('SetTemplateAsDefault.execute', () => {
 
   test('should set the default template for a collection', async () => {
     const templateName = `TestDefaultTemplate-${Date.now()}`
+    const collectionIdOrAlias = `setTemplateDefault${Date.now()}`
     const templateDto: CreateTemplateDTO = {
       name: templateName,
       isDefault: false,
@@ -55,21 +58,39 @@ describe('SetTemplateAsDefault.execute', () => {
       ]
     }
 
-    await createTemplate.execute(templateDto, collectionIdOrAlias)
-    const templatesAfterCreate = await getTemplatesByCollectionId.execute(collectionIdOrAlias)
-    const createdTemplate = templatesAfterCreate.find((template) => template.name === templateName)
+    let createdTemplateId: number | undefined
+    let collectionCreated = false
 
-    if (!createdTemplate) {
-      throw new Error('Created template was not found in collection templates.')
+    try {
+      await createCollectionViaApi(collectionIdOrAlias)
+      collectionCreated = true
+      await createTemplate.execute(templateDto, collectionIdOrAlias)
+      const templatesAfterCreate = await getTemplatesByCollectionId.execute(collectionIdOrAlias)
+      const createdTemplate = templatesAfterCreate.find(
+        (template) => template.name === templateName
+      )
+
+      if (!createdTemplate) {
+        throw new Error('Created template was not found in collection templates.')
+      }
+
+      createdTemplateId = createdTemplate.id
+
+      await setTemplateAsDefault.execute(createdTemplate.id, collectionIdOrAlias)
+
+      const templatesAfterSet = await getTemplatesByCollectionId.execute(collectionIdOrAlias)
+      const updatedTemplate = templatesAfterSet.find(
+        (template) => template.id === createdTemplate.id
+      )
+
+      expect(updatedTemplate?.isDefault).toBe(true)
+    } finally {
+      if (createdTemplateId !== undefined) {
+        await deleteDatasetTemplateViaApi(createdTemplateId)
+      }
+      if (collectionCreated) {
+        await deleteCollectionViaApi(collectionIdOrAlias)
+      }
     }
-
-    await setTemplateAsDefault.execute(createdTemplate.id, collectionIdOrAlias)
-
-    const templatesAfterSet = await getTemplatesByCollectionId.execute(collectionIdOrAlias)
-    const updatedTemplate = templatesAfterSet.find((template) => template.id === createdTemplate.id)
-
-    expect(updatedTemplate?.isDefault).toBe(true)
-
-    await deleteDatasetTemplateViaApi(createdTemplate.id)
   })
 })
