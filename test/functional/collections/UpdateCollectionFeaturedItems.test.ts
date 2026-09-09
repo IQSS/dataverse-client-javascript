3348e1cd7c8f98cf83e8c20c9abb2bed3c2b8e16
ErykKul <eryk.kulikowski@gmail.com>
2026-09-09 18:30:53 +0200

    Compare featured item HTML with a normalizer instead of an exact server-formatted string

--- test/functional/collections/UpdateCollectionFeaturedItems.test.ts
+++ test/functional/collections/UpdateCollectionFeaturedItems.test.ts
@@ -27,6 +27,7 @@ import {
   FeaturedItemType
 } from '../../../src/collections/domain/models/FeaturedItem'
 import { uploadFileViaApi } from '../../testHelpers/files/filesHelper'
+import { normalizeHtml } from '../../testHelpers/html/htmlNormalizer'
 import {
   deletePublishedDatasetViaApi,
   publishDatasetViaApi,
@@ -165,7 +166,9 @@ describe('execute', () => {
     expect(secondItemResponse.imageFileUrl).toBeUndefined()
     expect(secondItemResponse.imageFileName).toBeUndefined()
 
-    expect(thirdItemResponse.content).toEqual(EXPECTED_CONTENT_FIELD_WITH_ALL_TAGS)
+    expect(normalizeHtml(thirdItemResponse.content)).toEqual(
+      normalizeHtml(EXPECTED_CONTENT_FIELD_WITH_ALL_TAGS)
+    )
     expect(thirdItemResponse.displayOrder).toBe(newFeaturedItems[2].displayOrder)
     expect(thirdItemResponse.imageFileName).toEqual('featured-item-test-image-3.png')
     expect(thirdItemResponse.imageFileUrl).toContain(
